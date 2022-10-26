4979af17848b987fd0dd883938f8bb4ee765ee94
GDSC NIT Silchar <dscnits@gmail.com>
2022-10-26 20:11:03 +0530

    Revert "ADD: Router for pages"

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div>Amazing GDSC NIT Silchar Website</div>;
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Projects from "./pages/Projects/Projects";
-import Team from "./pages/Team/Team";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/projects",
-    element: <Projects />,
-  },
-  {
-    path: "/team",
-    element: <Team />,
-  },
-]);
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
