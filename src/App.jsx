0e617c3d693ec3032ef66887ebd3ed9d7fa3c6bb
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2022-10-26 19:40:41 +0530

    ADD: Router for pages

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div>Amazing GDSC NIT Silchar Website</div>;
-}
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
+import Team from "./pages/Team/Team";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/team",
+    element: <Team />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
