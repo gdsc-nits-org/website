9b335adbc7fa2c03bd1b56e59217ac36964078a9
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2022-10-27 15:53:33 +0530

    ADD: Added rudimentary navbar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
 import Team from "./pages/Team/Team";
+import Footer from "./components/Footer/Footer";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +25,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar />
     <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 );
