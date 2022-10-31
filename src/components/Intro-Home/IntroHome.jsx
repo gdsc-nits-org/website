1df92b7eca8125eb740364b7a825312e0e39e4df
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2022-10-31 19:35:22 +0530

    ADD: intro-content

--- src/components/Intro-Home/IntroHome.jsx
+++ src/components/Intro-Home/IntroHome.jsx
@@ -27,6 +27,101 @@ const IntroHome = () => {
               communities to showcase their talents and, imporve and gain new skillsets
             </p>
           </div>
+
+          <div className="web">
+            <div className="title">
+              <div className="logo"></div>
+              <div className="heading">Web Domain</div>
+            </div>
+            <p className="text">
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets.
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets
+            </p>
+          </div>
+
+          <div className="ui-ux">
+            <div className="title">
+              <div className="logo"></div>
+              <div className="heading">UI/UX Domain</div>
+            </div>
+            <p className="text">
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets.
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets
+            </p>
+          </div>
+
+          <div className="android">
+            <div className="title">
+              <div className="logo"></div>
+              <div className="heading">Android Domain</div>
+            </div>
+            <p className="text">
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets.
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets
+            </p>
+          </div>
+
+          <div className="flutter">
+            <div className="title">
+              <div className="logo"></div>
+              <div className="heading">Flutter Domain</div>
+            </div>
+            <p className="text">
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets.
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets
+            </p>
+          </div>
+
+          <div className="cloud">
+            <div className="title">
+              <div className="logo"></div>
+              <div className="heading">Cloud Domain</div>
+            </div>
+            <p className="text">
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets.
+              GDSC NIT Silchar is under Google Developer Students Club, which is a
+              worldwide developers community initiated by Google. Our GDSC prioritizes
+              solving real life problems that we as part of a society face in our daily
+              lives. Solving such problems brings opportunities for the budding developer
+              communities to showcase their talents and, imporve and gain new skillsets
+            </p>
+          </div>
         </div>
       </div>
       <div className="animated-navbar">
