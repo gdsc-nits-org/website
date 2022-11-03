3eacdab26af4fae74adb3ce209a57004c4b88672
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2022-11-04 04:38:25 +0530

    ADD: Videos for homepage

--- src/components/HomeVideos/HomeVideos.jsx
+++ src/components/HomeVideos/HomeVideos.jsx
@@ -3,7 +3,7 @@ import "./HomeVideos.scss";
 import TempVideos from "./TempVideos.json";
 
 function HomeVideos() {
-  const videoData = useState(TempVideos.video);
+  const [videoData] = useState(TempVideos.video);
   return (
     <div className="video-section">
       <div className="video-title">Video section</div>
@@ -13,7 +13,7 @@ function HomeVideos() {
             <div className="vid-wrapper">
               <div className="vid-container">
                 <iframe
-                  src="https://www.youtube.com/embed/eOVS1s3mJro"
+                  src={videoData.src}
                   title="YouTube video player"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
