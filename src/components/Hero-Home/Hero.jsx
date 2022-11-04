0ce818430045ba97a8ed25dfa6572663e31d617f
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2022-11-04 19:30:49 +0530

    UPDATE: About Us

--- src/components/Hero-Home/Hero.jsx
+++ src/components/Hero-Home/Hero.jsx
@@ -5,7 +5,19 @@ import scroll from "../../assets/images/Scroll-Down.png";
 const Hero = () => {
   return (
     <div className="home-hero">
-      <img src={scroll} alt="" className="scroll-gif" />
+      <div className="scroll-gif">
+        <img src={scroll} alt="" className="scroll-img" />
+        <div className="circle"></div>
+      </div>
+      <div className="text">
+        <div className="title gdsc">GDSC</div>
+        <div className="title nits">NIT Silchar</div>
+        <div className="twl">
+          <div className="tw">Together we</div>
+          <div className="learn">Learn</div>
+        </div>
+      </div>
+      <div className="hero-img"></div>
     </div>
   );
 };
