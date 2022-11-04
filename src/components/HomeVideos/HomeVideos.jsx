99ad07e668030b2b63460e81049d19bb40226424
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2022-11-04 22:30:42 +0530

    FIX: Syntax errors in HomeVideos.jsx

--- src/components/HomeVideos/HomeVideos.jsx
+++ src/components/HomeVideos/HomeVideos.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import "./HomeVideos.scss";
-import TempVideos from "./TempVideos.json";
+import tempVideos from "./TempVideos.json";
 
 function HomeVideos() {
-  const [videoData] = useState(TempVideos.video);
+  const [videoData] = useState(tempVideos.video);
   return (
     <div className="video-section">
       <div className="video-title">Video section</div>
       <div className="video-container">
         {videoData.map((videoData) => {
           return (
-            <div className="vid-wrapper">
+            <div key={videoData.id} className="vid-wrapper">
               <div className="vid-container">
                 <iframe
                   src={videoData.src}
                   title="YouTube video player"
-                  frameborder="0"
+                  frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowfullscreen
                 ></iframe>
