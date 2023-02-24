ec6429e3e877729f5b0beb977bfe92b67927f569
JOSU9435 <kamalnetra702@gmail.com>
2023-02-24 23:56:12 +0530

    UPD: react router v6.4 -> v6

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.scss";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
 import Team from "./pages/Team/Team";
@@ -9,41 +9,21 @@ import Footer from "./components/Footer/Footer";
 import ProjectsShow from "./components/ProjectsCard/Projectshow";
 import Page404 from "./pages/PageNotFound/Page404";
 import Navbar from "./components/Navbar/Navbar";
-// import { Router } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Navbar />
-        <Home />
-      </>
-    ), // Putting the navbar in the pages
-    // element: <Home />,
-  },
-  {
-    path: "*",
-    // element: <><Navbar/><Home /></>, // Putting the navbar in the pages
-    element: <Page404 />,
-  },
-  {
-    path: "/projects/",
-    element: <Projects />,
-  },
-  {
-    path: "/team",
-    element: <Team />,
-  },
-  {
-    path: "/projects/:projectID",
-    element: <ProjectsShow />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Footer />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/projects">
+          <Route index element={<Projects />} />
+          <Route path=":projectID" element={<ProjectsShow />} />
+        </Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
