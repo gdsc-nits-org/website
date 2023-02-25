0a7b1d6b96338d231240a1275809911de24ed300
ankit07an <ankit21_ug@civil.nits.ac.in>
2023-02-26 03:55:08 +0530

    projects lottie added

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,14 +1,29 @@
 import React from "react";
-
 import ProjectCard from "../../components/ProjectsCard/ProjectsCard";
 import "./Projects.scss";
+// import { useRef } from "react";
+// import "@lottiefiles/lottie-player";
+// import { create } from "@lottiefiles/lottie-interactivity";
+// import { useLayoutEffect } from "react";
+import Lottie from "react-lottie";
+import animationData from "../../assets/lottiefiles/roadmap.json";
 
 function Projects() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+  };
   return (
     <div>
       <header className="projects-header">
-        <h1 className="projects-title">Our</h1>
-        <h1 className="projects-title">Projects</h1>
+        <div className="projects-header-left">
+          <h1 className="projects-title">Our</h1>
+          <h1 className="projects-title">Projects</h1>
+        </div>
+        <div className="projects-header-right">
+          <Lottie options={defaultOptions} />
+        </div>
       </header>
       <main className="projects-main">
         <ProjectCard />
