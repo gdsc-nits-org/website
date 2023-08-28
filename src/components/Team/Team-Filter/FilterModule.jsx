9618dbdd9296c7dee062278c4d4e9791d15a3577
byt3h3ad <adhirajd007@gmail.com>
2023-08-28 11:33:08 +0530

    FIX: FilterModule code quality

--- src/components/Team/Team-Filter/FilterModule.jsx
+++ src/components/Team/Team-Filter/FilterModule.jsx
@@ -35,24 +35,36 @@ const FilterModule = forwardRef((props, ref) => {
       <div className={rotate.join(" ")}></div>
       {viewDropDown && (
         <ul className="dropDownContent">
-          {props.option1 && <div className="value" value={props.option1} onClick={onModuleValueChanged}>
-            {props.option1}
-          </div>}
-          {props.option2 && <div className="value" value={props.option2} onClick={onModuleValueChanged}>
-            {props.option2}
-          </div>}
-          {props.option3 && <div className="value" value={props.option3} onClick={onModuleValueChanged}>
-            {props.option3}
-          </div>}
-          {props.option4 && <div className="value" value={props.option4} onClick={onModuleValueChanged}>
-            {props.option4}
-          </div>}
-          {props.option5 && <div className="value" value={props?.option5} onClick={onModuleValueChanged}>
-            {props.option5}
-          </div>}
-          {props.option6 && <div className="value" value={props?.option6} onClick={onModuleValueChanged}>
-            {props.option6}
-          </div>}
+          {props.option1 && (
+            <div className="value" value={props.option1} onClick={onModuleValueChanged}>
+              {props.option1}
+            </div>
+          )}
+          {props.option2 && (
+            <div className="value" value={props.option2} onClick={onModuleValueChanged}>
+              {props.option2}
+            </div>
+          )}
+          {props.option3 && (
+            <div className="value" value={props.option3} onClick={onModuleValueChanged}>
+              {props.option3}
+            </div>
+          )}
+          {props.option4 && (
+            <div className="value" value={props.option4} onClick={onModuleValueChanged}>
+              {props.option4}
+            </div>
+          )}
+          {props.option5 && (
+            <div className="value" value={props?.option5} onClick={onModuleValueChanged}>
+              {props.option5}
+            </div>
+          )}
+          {props.option6 && (
+            <div className="value" value={props?.option6} onClick={onModuleValueChanged}>
+              {props.option6}
+            </div>
+          )}
         </ul>
       )}
     </div>
