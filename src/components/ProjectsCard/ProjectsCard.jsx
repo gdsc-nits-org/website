8093b9670cc201e3769e91b9b9c7512aa7468efd
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-09-10 20:18:55 +0530

    fixed responsive issues in events page, overall layout

--- src/components/ProjectsCard/ProjectsCard.jsx
+++ src/components/ProjectsCard/ProjectsCard.jsx
@@ -12,38 +12,40 @@ function ProjectCard() {
             <div className="project-card-header">
               <div className="project-card-project-status-container">
                 <h2 className="project-card-project-name">{cardInfo.title}</h2>
-                {cardInfo.link ? (
-                  <a
-                    href={cardInfo.link}
-                    className="project-card-project-status"
-                    style={{
-                      backgroundColor: "#4a90e2",
-                    }}
-                    title={cardInfo.title}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {cardInfo.Status}
-                  </a>
-                ) : (
-                  <span
-                    className="project-card-project-status"
-                    style={{
-                      backgroundColor: cardInfo.Status === "Live" ? "#4a90e2" : "#3CBA54",
-                    }}
-                  >
-                    {cardInfo.Status}
-                  </span>
-                )}
-              </div>
-              <div className="project-card-project-img-container">
-                <img className="project-card-project-img" src={cardInfo.logo} alt="" />
+                <div className="project-card-project-img-container">
+                  <img className="project-card-project-img" src={cardInfo.logo} alt="" />
+                </div>
               </div>
             </div>
-            <p className="project-card-content">{cardInfo.description}</p>
+            <div className="project-status">
+              {cardInfo.link ? (
+                <a
+                  href={cardInfo.link}
+                  className="project-card-project-status"
+                  style={{
+                    backgroundColor: "#4a90e2",
+                  }}
+                  title={cardInfo.title}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {cardInfo.Status}
+                </a>
+              ) : (
+                <span
+                  className="project-card-project-status"
+                  style={{
+                    backgroundColor: cardInfo.Status === "Live" ? "#4a90e2" : "#3CBA54",
+                  }}
+                >
+                  {cardInfo.Status}
+                </span>
+              )}
+              <p className="project-card-content">{cardInfo.description}</p>
+            </div>
             <div className="project-card-footer">
               <Link to={`/projects/${index}`}>
-                <div className="project-card-details">View Details</div>
+                <button className="project-card-details">View Details</button>
               </Link>
             </div>
           </div>
