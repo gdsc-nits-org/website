212d74f6a940020ef2ccf489b34d09cfff505dc1
barnilsarma <barnilsarma@gmail.com>
2023-12-09 01:09:00 +0530

    redirect to blog page

---
+++ src/components/Blog.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+const Blog = () => {
+    return (
+        useEffect(() => {
+            window.location.href = "https://gdscnits.hashnode.dev/"
+        }, [])
+    )
+}
+export default Blog;
